0e1c4ce62514ec92b1a89fdc3ad1543ffc5187c5
adhikari-ishan <adhikariishan02@gmail.com>
2026-05-27 16:47:09 +0545

    board made day 2

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
 const board = document.querySelector(".board");
-const blockheight = 30;
-const blockwidth = 30;
+const blockheight = 45;
+const blockwidth = 45;
 
 const cols = Math.floor(board.clientWidth / blockwidth);
 const rows = Math.floor(board.clientHeight / blockheight);
 
 
-for(let i=0; i<rows*cols; i++){
-    const block = document.createElement("div");
-    block.classList.add("block");
-    board.appendChild(block); 
+// for(let i=0; i<rows*cols; i++){
+//     const block = document.createElement("div");
+//     block.classList.add("block");
+//     board.appendChild(block); 
+// }
+
+
+for (let row=0; row<=rows; row++){
+    for (let col=0; col<=cols; col++){
+        const block = document.createElement("div");
+        block.classList.add("block");
+        board.appendChild(block);
+    }
 }
